17ba3d78f0a353fbf94bcde55f17fbbc8abd46d8
Austin Keller <austinkllr@gmail.com>
2022-09-21 20:09:19 -0700

    created NavBar component, which includes buttons for Home and Shop, as well as Cart

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <button>Home</button>
+      <button>Shop</button>
+      <span>Cart</span>
+    </div>
+  );
+};
+
+export default NavBar;
