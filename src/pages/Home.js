605fbbc8589626bd9144c2f4198fa86723279192
Austin Keller <austinkllr@gmail.com>
2022-09-21 21:04:41 -0700

    added css module to home page, basic styling for header content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 
+import styles from "../styles/Home.module.css";
+
 const Home = () => {
   return (
     <div>
       <NavBar />
-      <div>
+      <div className={styles.container}>
         <h1>Welcome to the best climbing shop on the planet</h1>
         <h3>From beginners to experts, we have what you need</h3>
         <button>Shop</button>
