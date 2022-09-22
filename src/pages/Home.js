982721d7811b4532b8e50cb4516afc9deb365ce0
Austin Keller <austinkllr@gmail.com>
2022-09-21 21:50:37 -0700

    added home background image, changed header to flex

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,9 @@ import styles from "../styles/Home.module.css";
 
 const Home = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <NavBar />
-      <div className={styles.container}>
+      <div className={styles.header}>
         <h1>Welcome to the best climbing shop on the planet</h1>
         <h3>From beginners to experts, we have what you need</h3>
         <button>Shop</button>
