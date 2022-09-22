55bef6404ff99df07a2b0a9735751593cc518a40
Austin Keller <austinkllr@gmail.com>
2022-09-21 20:04:14 -0700

    added title and header div to fill out home page; no styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div>The Crag</div>
+      {/* NavBar */}
+      <div>
+        <h1>Welcome to the best climbing shop on the planet</h1>
+        <h3>From beginners to experts, we have what you need</h3>
+        <button>Shop</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
