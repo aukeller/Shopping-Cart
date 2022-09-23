494fedf02077dbfc3e2323936e54b2b173029d10
Austin Keller <austinkllr@gmail.com>
2022-09-22 20:35:43 -0700

    styled button and added link to shop page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,8 @@ import NavBar from "../components/NavBar";
 
 import styles from "../styles/Home.module.css";
 
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <div className={styles.container}>
@@ -10,7 +12,9 @@ const Home = () => {
       <div className={styles.header}>
         <h1>Welcome to the best climbing shop on the planet</h1>
         <h3>From beginners to experts, we have what you need</h3>
-        <button>Shop</button>
+        <button>
+          <Link to="/shop">SHOP NOW</Link>
+        </button>
       </div>
     </div>
   );
