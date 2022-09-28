6a1e226052266c94b5290374ee9098afa4205907
Austin Keller <austinkllr@gmail.com>
2022-09-27 18:08:51 -0700

    added images and product info data files to be used by Product components

---
+++ src/assets/product-data/images/index.js
@@ -0,0 +1,41 @@
+import shoes1 from "./Black diamond climbing Shoes.jpg";
+import shoes2 from "./Zenit Climbing Shoes.jpg";
+import shoes3 from "./Zenit Womens Shoes.jpg";
+import essentials1 from "./essential1.jpg";
+import essentials2 from "./essential2.jpg";
+import essentials3 from "./essential3.jpg";
+import gear1 from "./gear1.jpg";
+import gear2 from "./gear2.jpg";
+import gear3 from "./gear3.jpg";
+import harness1 from "./harness1.jpg";
+import harness2 from "./harness2.jpg";
+import harness3 from "./harness3.jpg";
+import rope1 from "./rope1.jpg";
+import rope2 from "./rope2.jpg";
+import rope3 from "./rope3.jpg";
+import sling1 from "./sling1.jpg";
+import sling2 from "./sling2.jpg";
+import sling3 from "./sling3.jpg";
+
+const images = {
+  shoes1,
+  shoes2,
+  shoes3,
+  essentials1,
+  essentials2,
+  essentials3,
+  gear1,
+  gear2,
+  gear3,
+  harness1,
+  harness2,
+  harness3,
+  rope1,
+  rope2,
+  rope3,
+  sling1,
+  sling2,
+  sling3,
+};
+
+export default images;
---
+++ src/assets/product-data/productDetails.js
@@ -0,0 +1,96 @@
+import images from "./images";
+
+const productInfo = {
+  shoes1: {
+    name: "La Sportiva Zenit Climbing Shoes - Men's",
+    price: "$81.83",
+    image: images.shoes1,
+  },
+  shoes2: {
+    name: "Black Diamond - Zone Climbing Shoes",
+    price: "$73.73",
+    image: images.shoes2,
+  },
+  shoes3: {
+    name: "La Sportiva Zenit Climbing Shoes - Women's",
+    price: "$81.83",
+    image: images.shoes3,
+  },
+  essentials1: {
+    name: "Metolius Climbing Tape",
+    price: "$4.95",
+    image: images.essentials1,
+  },
+  essentials2: {
+    name: "Metolius Finger Tape - Package of 2",
+    price: "$4.95",
+    image: images.essentials2,
+  },
+  essentials3: {
+    name: "Burt's Bees Hand Salve Tin - 3 oz.",
+    price: "$10.00",
+    image: images.essentials3,
+  },
+  gear1: {
+    name: "Petzl GRIGRI Belay Device",
+    price: "$99.95",
+    image: images.gear1,
+  },
+  gear2: {
+    name: "Black Diamond - Big Air XP Belay Device Package",
+    price: "$34.95",
+    image: images.gear2,
+  },
+  gear3: {
+    name: "Petzl - Djinn Axess Quickdraw Set",
+    price: "$15.95",
+    image: images.gear3,
+  },
+  harness1: {
+    name: "PetzlCorax LT Harness - Men's",
+    price: "$64.95",
+    image: images.harness1,
+  },
+  harness2: {
+    name: "Momentum Harness Package - Women's",
+    price: "$99.95",
+    image: images.harness2,
+  },
+  harness3: {
+    name: "Momentum Harness - Men's",
+    price: "$64.95",
+    image: images.harness3,
+  },
+  rope1: {
+    name: "9.9mm Non-Dry Rope",
+    price: "$129.95",
+    image: images.rope1,
+  },
+  rope2: {
+    name: "E-Z Bend Sport 11 mm Non-Dry Static Rope",
+    price: "$0.95",
+    image: images.rope2,
+  },
+  rope3: {
+    name: "VR9 9.8 mm Dry-Core Rope",
+    price: "$119.95",
+    image: images.rope3,
+  },
+  sling1: {
+    name: "Metolious Sling - 11mm",
+    price: "$5.95",
+    image: images.sling1,
+  },
+  sling2: {
+    name: "Metolious 18mm Nylon Sling",
+    price: "$3.95",
+    image: images.sling2,
+  },
+  sling3: {
+    name: "BlueWater 7mm x 30 ft. Accessory Cord",
+    price: "$15.00",
+    image: images.sling3,
+  },
+};
+
+export default productInfo;
