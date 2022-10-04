7450b224f602999db09c06406f7a15924ee8f02a
Austin Keller <austinkllr@gmail.com>
2022-10-03 21:41:47 -0700

    shop page renders list of components using product data

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,10 +1,29 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import Product from "../components/Product";
+
+import productInfo from "../assets/product-data/productDetails";
 
 const Shop = () => {
+  const generateProductList = () => {
+    const productComponents = [];
+    for (let product in productInfo) {
+      let name = productInfo[product].name;
+      let price = productInfo[product].price;
+      let image = productInfo[product].image;
+
+      productComponents.push(
+        <Product key={name} name={name} price={price} image={image} />
+      );
+    }
+
+    return productComponents;
+  };
+
   return (
     <div>
       <NavBar />
+      <div>{generateProductList()}</div>
     </div>
   );
 };
