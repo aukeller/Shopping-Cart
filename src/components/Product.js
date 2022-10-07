9681a84c20678137b4a5da6eff87d99b56908e4a
Austin Keller <austinkllr@gmail.com>
2022-10-06 20:38:41 -0700

    added styling to Product components, increased row gap on product list

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,17 @@
 import React from "react";
+import styles from "../styles/Product.module.css";
 
 const Product = (props) => {
   return (
-    <div>
-      <img src={props.image} alt="" />
-      <h3>{props.name}</h3>
-      <span>{props.price}</span>
-      <button>Add to Cart</button>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <img src={props.image} alt="" />
+      </div>
+      <div className={styles.infoContainer}>
+        <h3>{props.name}</h3>
+        <span>{props.price}</span>
+        <button>Add to Cart</button>
+      </div>
     </div>
   );
 };
