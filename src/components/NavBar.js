7c1a19146ff413ee4e191b7f0a83eab920d1320e
Austin Keller <austinkllr@gmail.com>
2022-11-21 21:23:16 -0800

    refactored minor navbar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,26 @@
 import React from "react";
 import styles from "../styles/NavBar.module.css";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faBagShopping } from "@fortawesome/free-solid-svg-icons";
 
 const NavBar = ({ orderCount }) => {
   return (
     <div className={styles.container}>
-      <div>The Crag</div>
+      <div>
+        <Link to="/">The Crag</Link>
+      </div>
       <ul className={styles.nav}>
         <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
+          <FontAwesomeIcon icon={faBagShopping} size="xs" />
           <Link to="/shop">Shop</Link>
         </li>
         <li>
           <Link to="/cart">
-            Cart <span>{orderCount}</span>
+            <FontAwesomeIcon icon={faCartShopping} size="xs" />
+            <span>{orderCount}</span>
+            Cart
           </Link>
         </li>
       </ul>
